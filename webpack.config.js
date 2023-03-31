af1599bff94995854a0f27abf8ed1f8eeece9bf7
lxxxv5 <1490769790@qq.com>
2023-03-31 13:14:48 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+}
