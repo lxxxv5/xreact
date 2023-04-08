0b1e82b27787212437d3a741290b9d27e9e334b2
lxxxv5 <1490769790@qq.com>
2023-04-08 22:33:02 +0800

    wip HostComponent onClick

--- example/index.jsx
+++ example/index.jsx
@@ -3,39 +3,40 @@ import ReactDOM from 'react-dom'
 
 import './index.less'
 
-function LiItem({ toolName }) {
-  return <li>{toolName}</li>
-}
+// function LiItem({ toolName }) {
+//   return <li>{toolName}</li>
+// }
 
-function ToolList({ list }) {
-  return (
-    <ul className="title" style={{ fontSize: 18 }}>
-      {list.map(item => (
-        <LiItem toolName={item} />
-      ))}
-    </ul>
-  )
-}
+// function ToolList({ list }) {
+//   return (
+//     <ul className="title" style={{ fontSize: 18 }}>
+//       {list.map(item => (
+//         <LiItem toolName={item} />
+//       ))}
+//     </ul>
+//   )
+// }
 
 class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      tools: [
-        ['react', 'vue', 'angular'],
-        ['webpack', 'vite', 'rollup'],
-      ],
-    }
-  }
+  // constructor(props) {
+  //   super(props)
+  //   this.state = {
+  //     tools: [
+  //       ['react', 'vue', 'angular'],
+  //       ['webpack', 'vite', 'rollup'],
+  //     ],
+  //   }
+  // }
   render() {
-    const { tools } = this.state
-    return (
-      <>
-        {tools.map(item => (
-          <ToolList list={item} />
-        ))}
-      </>
-    )
+    // const { tools } = this.state
+    return <div className="title">xreact</div>
+    // return (
+    //   <>
+    //     {tools.map(item => (
+    //       <ToolList list={item} />
+    //     ))}
+    //   </>
+    // )
   }
 }
 
